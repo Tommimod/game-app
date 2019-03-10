e12bf0504887247680f335816a6974d4f517af7a
Tommimod <38009019+Tommimod@users.noreply.github.com>
2019-03-10 19:01:59 +0300

    build

---
+++ build/precache-manifest.6f4f8cc76e75336e05f9a451301c8328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d71bf0a264288e759374",
+    "url": "/static/js/main.d71bf0a2.chunk.js"
+  },
+  {
+    "revision": "b6652a00f774c5613634",
+    "url": "/static/js/2.b6652a00.chunk.js"
+  },
+  {
+    "revision": "d71bf0a264288e759374",
+    "url": "/static/css/main.78a7bc1f.chunk.css"
+  },
+  {
+    "revision": "95cb6082c75a5462102ef62af6df9e6f",
+    "url": "/index.html"
+  }
+];
